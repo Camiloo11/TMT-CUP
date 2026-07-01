c2dc94f8b04175cd4abf4e9bc9a14d42a1106deb
Camilo Ustariz <camiloustarizperez@gmail.com>
2026-06-30 19:35:20 -0500

    Add Player model with relation to Team

---
+++ prisma/migrations/20260701001714_add_player/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Player" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "number" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "teamId" INTEGER NOT NULL,
+
+    CONSTRAINT "Player_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Player" ADD CONSTRAINT "Player_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
